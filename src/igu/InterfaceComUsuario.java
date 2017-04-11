dc8f626fe2a6106944ba82e7f6ab38f52418432d
renatommps <renato.kun.xd@gmail.com>
2017-04-11 17:54:26 -0300

    Initial commit

---
+++ src/igu/InterfaceComUsuario.java
@@ -0,0 +1,14 @@
+package igu;
+
+import java.util.ArrayList;
+import java.util.List;
+import logica.Jogador;
+
+public class InterfaceComUsuario {
+
+	private List<Jogador> jogadores;
+
+	public InterfaceComUsuario() {
+		jogadores = new ArrayList<Jogador>();
+	}
+}
---
+++ src/principal/Porrinha.java
@@ -0,0 +1,11 @@
+package principal;
+
+import igu.InterfaceComUsuario;
+
+public class Porrinha {
+
+	public static void main(String[] args) {
+		new InterfaceComUsuario();
+	}
+
+}
