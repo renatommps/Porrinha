f858c931241d45b3ad43b14f56c780067978a9c5
Lucas Roman <lfrfinger@gmail.com>
2017-04-17 14:45:43 -0300

    Decker basura

--- src/logica/EstadoPorrinha.java
+++ src/logica/EstadoPorrinha.java
@@ -11,7 +11,7 @@ public class EstadoPorrinha implements EstadoArvore {
 		this.maxJogadores = maxJogadores;
 		System.out.println(maoAposta);
 	}
-	
+
 	@Override
 	public ArrayList<EstadoArvore> transicoes() {
 		ArrayList<EstadoArvore> ret = new ArrayList<EstadoArvore>();
@@ -21,61 +21,49 @@ public class EstadoPorrinha implements EstadoArvore {
 			sum += jogador.get(0);
 		}
 		for (int i = 0; i <= sum; ++i) {
-			ArrayList<ArrayList<Integer>> novo = new ArrayList<ArrayList<Integer>>();
-			boolean add = true;
-			for (int j = 0; j < maoAposta.size(); ++j) {
-				ArrayList<Integer> valores = new ArrayList<Integer>();
-				int toAdd = 0;
+			if (unique(jogadorAtual, i)) {
 
-				if (j == jogadorAtual) {
-					add = unique(j, i);
-					toAdd = i;
-				} else {
-					toAdd = maoAposta.get(j).get(1);
-				}
-
-				valores.add(maoAposta.get(j).get(0));
-				valores.add(toAdd);
-				novo.add(valores);
-			}
-			if (add) {
+				ArrayList<ArrayList<Integer>> novo = new ArrayList<ArrayList<Integer>>(maoAposta);
+				ArrayList<Integer> apostaJogadorAtual = novo.get(jogadorAtual);
+				apostaJogadorAtual.set(1, i);
+				novo.set(jogadorAtual, apostaJogadorAtual);
 				ret.add(new EstadoPorrinha(novo, jogados, proxJogador, maxJogadores));
 			}
 		}
 		return ret;
 	}
-	
+
 	public int numJogadores() {
 		return maxJogadores;
 	}
 
 	@Override
-	public boolean isFinal() { 
-		for(ArrayList<Integer> jogador : maoAposta) {
-			if(jogador.get(1) == -1) {
+	public boolean isFinal() {
+		for (ArrayList<Integer> jogador : maoAposta) {
+			if (jogador.get(1) == -1) {
 				return false;
 			}
 		}
 		return true;
 	}
-	
+
 	@Override
 	public ArrayList<Double> visit(Euristica euristica) {
-		return euristica.accept(this);		
+		return euristica.accept(this);
 	}
-	
+
 	public ArrayList<ArrayList<Integer>> getMaoAposta() {
 		return maoAposta;
 	}
-	
+
 	public int jogadosPeloAtual() {
 		return jogados;
 	}
-	
+
 	public int getJogadorAtual() {
 		return jogadorAtual;
 	}
-	
+
 	private boolean unique(int curIndex, int value) {
 		for (int i = 0; i < curIndex; ++i) {
 			if (maoAposta.get(i).get(1) == value) {
@@ -89,6 +77,5 @@ public class EstadoPorrinha implements EstadoArvore {
 	private int jogados;
 	private int jogadorAtual;
 	private int maxJogadores;
-	
-	
+
 }
