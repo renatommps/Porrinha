9e38e3a2f75ccf8779ccbeadb5227720e70c46d4
renatommps <renato.kun.xd@gmail.com>
2017-04-20 13:57:52 -0300

    teste

--- src/logica/StatePlayersInstantiation.java
+++ src/logica/StatePlayersInstantiation.java
@@ -13,11 +13,8 @@ public class StatePlayersInstantiation implements State {
 	public void process(StateContext context, ControladorDeJogo controladorDeJogo) {
 		FabricaDeJogador fabricaDeJogador = new FabricaDeJogador();
 
-/*		// lista com todos os tipos de estragégias criadas para o jogo
+		// lista com todos os tipos de estragégias criadas para o jogo
 		List<String> estrategias = Arrays.asList("ALEATORIO", "MINMAX");
-
-		// inicializa o index da lista de estrategias
-		int estrategiasListIndex = 0;
 
 		// numero total de estragegias criadas
 		int numeroDeEstrategias = estrategias.size();
@@ -25,19 +22,19 @@ public class StatePlayersInstantiation implements State {
 		// numero total de jogadores IA a serem criados
 		int numeroDeJogadoresIA = controladorDeJogo.getNumeroDeJogadoresIA();
 
-		
+		/*
 		 * instancia a quantidade de jogadores IA pedidos, variando os tipos de
 		 * estragégiasa serem criadas percorrendo a lista com as estrategias
 		 * feitas. Se chegar no final da lista, volta para o comeco dela
-		 
+		 */
 		for (int i = 0; i < numeroDeJogadoresIA; i++) {
-			
-		}*/
-		
-		
+			// percorre circularmente a lista de estratégias
+			int estrategiasListIndex = i % numeroDeEstrategias;
 
-		for (int i = 0; i < controladorDeJogo.getNumeroDeJogadoresIA(); i++) {
-			Jogador novoJogadorIA = fabricaDeJogador.getJogadorIA(Integer.toString(i), "minmax");
+			// identificador da estratégia utilizada
+			String estrategia = estrategias.get(estrategiasListIndex);
+
+			Jogador novoJogadorIA = fabricaDeJogador.getJogadorIA(Integer.toString(i), estrategia);
 			controladorDeJogo.addJogador(novoJogadorIA);
 		}
 
